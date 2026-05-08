8a3fccb0b577a0acf207c1305b41265e9887ff37
josephODL <josephc@overdrivefilms.com>
2026-05-08 12:27:35 +0930

    Add files via upload

---
+++ assets/clients/manifest.js
@@ -0,0 +1,23 @@
+/*
+ * Drop logo files into this folder (assets/clients/), then add an entry below.
+ * The site will only display logos whose files are actually present.
+ *
+ * Supported formats: .png  .jpg  .svg  .webp
+ *
+ * Example entry:
+ *   { name: 'Client Name', file: 'filename.png' }
+ */
+window.OD_CLIENTS = [
+  { name: '44',                    file: '44.jpg' },
+  { name: 'AAA',                   file: 'AAA.jpg' },
+  { name: 'ABC',                   file: 'ABC.jpg' },
+  { name: 'AFC',                   file: 'AFC.jpg' },
+  { name: 'CarClew',               file: 'CarClew.jpg' },
+  { name: 'Ford',                  file: 'Ford.jpg' },
+  { name: 'Home Affairs',          file: 'Home Affairs.jpg' },
+  { name: 'NSS',                   file: 'NSS.jpg' },
+  { name: 'New Ground Collective', file: 'New Ground Copllective.jpg' },
+  { name: 'She Digital',           file: 'She Digital.jpg' },
+  { name: 'Society',               file: 'Society.jpg' },
+  { name: 'Toppa',                 file: 'Toppa.jpg' },
+];
